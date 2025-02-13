79be5ef02739c96504cf6f08b958e398261f041e
AbdulsispnShakoor <shakoor.sispn@gmail.com>
2025-02-14 02:53:32 +0500

    feat: async handler added for catching try catch errors

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+// Wraps async functions and passes errors to Express error middleware
+const asyncHandler =
+  /*eslint-disable*/
+
+    (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+    (req: Request, res: Response, next: NextFunction) =>
+      Promise.resolve(fn(req, res, next)).catch(next);
+
+export default asyncHandler;
